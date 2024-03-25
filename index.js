86e5c917c6443352e628026dbfcc72f192eae446
OscarFerBarrera <osjamfer@hotmail.com>
2024-03-25 23:24:24 +0100

    Crud creado y las consultas en postam.

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const express = require("express");
 const { connect } = require("./db.js");
 connect();
 
-// Modelos
-const { Book } = require("./models/Book.js");
+// Routes
+const { bookRouter } = require("./routes/book.routes.js");
 
 // Creamos router de expres
 const PORT = 3000;
@@ -16,66 +16,15 @@ const router = express.Router();
 server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
 
-// Rutas
+// Ruta Home
 router.get("/", (req, res) => {
-  res.send("Esta es la home de nuestra API");
+  res.send("Esta es la home de nuestra API de Libros");
 });
 
-router.get("/book", (req, res) => {
-  Book.find()
-    .then((books) => res.json(books))
-    .catch((error) => res.status(500).json(error));
-});
-
-router.get("/book/:id", (req, res) => {
-  const id = req.params.id;
-
-  Book.findById(id)
-    .then((book) => {
-      if (book) {
-        res.json(book);
-      } else {
-        res.status(404).json({});
-      }
-    })
-    .catch((error) => res.status(500).json(error));
-});
-
-router.get("/book/title/:title", async (req, res) => {
-  const title = req.params.title;
-
-  try {
-    // Busqueda exacta
-    // const user = await User.find({ firstName: name });
-
-    const book = await Book.find({ title: new RegExp("^" + title.toLowerCase(), "i") });
-    if (book?.length) {
-      res.json(book);
-    } else {
-      res.status(404).json([]);
-    }
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
-
-// Endpoint de creación de usuarios
-router.post("/book", async (req, res) => {
-  try {
-    const book = new Book({
-      title: req.body.title,
-      author: req.body.author,
-      pages: req.body.pages,
-    });
-
-    const createdBook = await book.save();
-    return res.status(201).json(createdBook);
-  } catch (error) {
-    res.status(500).json(error);
-  }
-});
-
+// Usamos las rutas
 server.use("/", router);
+server.use("/book", bookRouter);
+
 server.listen(PORT, () => {
   console.log(`Server levantado en el puerto ${PORT}`);
 });
